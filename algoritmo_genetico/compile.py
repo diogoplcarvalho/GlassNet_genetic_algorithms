590be636b0b79c5703f58955e0a75a34d40db592
David <jose23038@ilum.cnpem.br>
2024-05-26 21:22:08 -0300

    Compilação do notebook

---
+++ algoritmo_genetico/compile.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+from glasspy.predict import GlassNet
+from glasspy.data import SciGlass
+from functools import partial
+ 
+from funcoes import cria_populacao_compostos as cria_populacao
+from funcoes import funcao_objetivo_pop as funcao_objetivo_pop
+from funcoes import selecao_torneio_min as funcao_selecao
+from funcoes import cruzamento_ponto_duplo as funcao_cruzamento
+from funcoes import mutacao_sucessiva as funcao_mutacao_1
+from funcoes import mutacao_simples as funcao_mutacao_2
+
+df_compounds_and_prices = pd.read_csv('../analise_exploratoria/Prices.csv',sep=',')
+
+COMPOUNDS = list(df_compounds_and_prices['Oxide Coumpounds'])
+PRECOS = list(df_compounds_and_prices['Price per gram(dolar/gram)'])
+NUM_COMPOUNDS = len(COMPOUNDS)
+VALOR_MAX_COMPOUNDS = 100
+
+TAMANHO_POPULACAO = 300
+NUM_GERACOES = 2
+CHANCE_DE_CRUZAMENTO = 0.5
+CHANCE_DE_MUTACAO = 0.05
+CHANCE_DE_MUTACAO_POR_GENE = 0.25
+TAMANHO_TORNEIO = 3
+
+source = SciGlass()
+MODEL = GlassNet()
+
+funcao_objetivo = partial(funcao_objetivo_pop, lista_de_compostos=COMPOUNDS, lista_de_precos=PRECOS, modelo=MODEL)
+
+populacao = cria_populacao(TAMANHO_POPULACAO, NUM_COMPOUNDS, VALOR_MAX_COMPOUNDS)
+
+hall_da_fama = []
+
+geracoes = np.arange(1,NUM_GERACOES+1,1)
+melhores_das_geracoes = []
+
+for n in range(NUM_GERACOES):
+    print(f'Geração {n}', end='\r') 
+    
+    # Seleção
+    fitness = funcao_objetivo(populacao)        
+    selecionados = funcao_selecao(populacao, fitness, TAMANHO_TORNEIO)
+    
+    # Cruzamento
+    proxima_geracao = []
+    for pai, mae in zip(selecionados[::2], selecionados[1::2]):
+        individuo1, individuo2 = funcao_cruzamento(pai, mae, CHANCE_DE_CRUZAMENTO)
+        proxima_geracao.append(individuo1)
+        proxima_geracao.append(individuo2)
+    
+    # Mutação
+    funcao_mutacao_1(proxima_geracao, CHANCE_DE_MUTACAO, CHANCE_DE_MUTACAO_POR_GENE, VALOR_MAX_COMPOUNDS)
+    funcao_mutacao_2(proxima_geracao, CHANCE_DE_MUTACAO, VALOR_MAX_COMPOUNDS)
+        
+    # Atualização do hall da fama
+    fitness = funcao_objetivo(proxima_geracao)
+        
+    menor_fitness = min(fitness)
+    indice = fitness.index(menor_fitness)
+    hall_da_fama.append(proxima_geracao[indice])    
+    melhores_das_geracoes.append(menor_fitness)
+    # Encerramento
+    populacao = proxima_geracao
+    
+    
+fitness = funcao_objetivo(hall_da_fama)
+menor_fitness = min(fitness)
+indice = fitness.index(menor_fitness)
+melhor_individuo_observado = hall_da_fama[indice]
+
+melhor_individuo_observado
+    
+    
+fitness = funcao_objetivo(hall_da_fama)
+maior_fitness = max(fitness)
+indice = fitness.index(maior_fitness)
+melhor_individuo_observado = hall_da_fama[indice]
+
+
+dict_composition = dict(zip(COMPOUNDS, melhor_individuo_observado))
+predicao = MODEL.predict(dict_composition)
+
+from funcoes import preco_composicao
+
+preco = preco_composicao(melhor_individuo_observado, PRECOS)
+
+print(f"Preço: {preco}")
+print(f"Módulo de Young: {float(predicao['YoungModulus'].iloc[0])}")
+print(f"Microdureza: {float(predicao['Microhardness'].iloc[0])}")
+
+dicionario= {
+   'Preço':preco,
+   'Módulo de Young': float(predicao['YoungModulus'].iloc[0]),
+   'Microdureza': float(predicao['Microhardness'].iloc[0])
+
+}
+
+pd.DataFrame(dicionario).to_excel('dados_melhor_candidato')
+evolucao = pd.DataFrame()
+evolucao['geracoes'] = geracoes
+evolucao['score'] = melhores_das_geracoes
